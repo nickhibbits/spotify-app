b0f82275cd31f17eccb2029e26363c471b4b3eef
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-12-31 14:47:25 -0800

    init react app, pass auth tokens via query params

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get("access_token");
+    const refreshToken = urlParams.get("refresh_token");
+  }, []);
+
+  return (
+    <div className="App">
+      <a className="App-link" href="http://localhost:8888/login">
+        Login to Spotify
+      </a>
+    </div>
+  );
+}
+
+export default App;
