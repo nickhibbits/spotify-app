6114c88c5ae310bd21e1fddf7e30ac26a57e553a
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-01-11 18:16:20 -0800

    Add composition style classes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,7 @@ import {
   getCurrentUserPlaylists,
   getTopArtists,
 } from "../spotify";
+import { SectionWrapper, ArtistsGrid } from "../components";
 import { StyledHeader } from "../styles";
 
 const Profile = () => {
@@ -60,6 +61,16 @@ const Profile = () => {
               </div>
             </div>
           </StyledHeader>
+          {topArtists && (
+            <main>
+              <SectionWrapper
+                title="Top artists this month"
+                seeAllLink="/top-artists"
+              >
+                <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+            </main>
+          )}
         </>
       )}
     </>
